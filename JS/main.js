69061b0f56b71e81b5f43b86e02acc648ab9c1d2
monchi316 <monchi316@gmail.com>
2020-06-01 15:40:02 +0900

    課題①-3完了

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,23 @@
 'user strict'
+
+const tasks = ['掃除', '買い物', '散歩'];
+
+let answer = 0;
+
+while (answer < 100) {
+
+
+  tasks.push(prompt('タスクを入力して下さい'));
+
+  prompt('「確認,追加,削除,終了」の4つのいずれかを入力して下さい');
+  alert('新しいタスクを追加しました');
+
+  console.log('========================');
+  console.log('現在持っているタスク');
+  console.log('========================');
+
+  for (let i = 0; i < tasks.length; i++) {
+    console.log(`${i}:${tasks[i]}`);
+  }
+  answer++;
+}
